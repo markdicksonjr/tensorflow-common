214960182144b6d4fec4a348f13eb861f5dac4fc
Mark Dickson Jr <markdicksonjr@gmail.com>
2019-12-29 21:55:45 -0500

    Some minor inference tools for object detection

--- object_detection.go
+++ object_detection.go
@@ -1,6 +1,10 @@
 package tensorflow_common
 
-import "sort"
+import (
+	tf "github.com/tensorflow/tensorflow/tensorflow/go"
+	"log"
+	"sort"
+)
 
 type ObjectDetectionResult struct {
 	Probability float32
@@ -8,6 +12,14 @@ type ObjectDetectionResult struct {
 	Label       string
 	Index       int
 	Class       float32
+}
+
+type ObjectDetectionOperationMappings struct {
+	ImageTensor      string
+	DetectionBoxes   string
+	DetectionScores  string
+	DetectionClasses string
+	NumDetections    string
 }
 
 func RawSlicesToObjectDetectionResult(
@@ -34,6 +46,63 @@ func RawSlicesToObjectDetectionResult(
 	})
 
 	return results
+}
+
+// PerformObjectDetectionInference runs the image through the model
+func PerformObjectDetectionInference(
+	input *tf.Tensor,
+	graph *tf.Graph,
+	session *tf.Session,
+	mappings ...ObjectDetectionOperationMappings,
+) (probabilities, classes []float32, boxes [][]float32, err error) {
+	var resolvedMappings ObjectDetectionOperationMappings
+
+	if len(mappings) > 0 {
+		resolvedMappings = mappings[0]
+	} else {
+		resolvedMappings = ObjectDetectionOperationMappings{
+			ImageTensor:      "image_tensor",
+			DetectionBoxes:   "detection_boxes",
+			DetectionScores:  "detection_scores",
+			DetectionClasses: "detection_classes",
+			NumDetections:    "num_detections",
+		}
+	}
+
+	// get all the input and output operations
+	inputOp := graph.Operation(resolvedMappings.ImageTensor)
+	ops := graph.Operations()
+	for _, v := range ops {
+		log.Println(v.Name())
+	}
+
+	// get the output ops
+	o1 := graph.Operation(resolvedMappings.DetectionBoxes)
+	o2 := graph.Operation(resolvedMappings.DetectionScores)
+	o3 := graph.Operation(resolvedMappings.DetectionClasses)
+	o4 := graph.Operation(resolvedMappings.NumDetections)
+
+	output, err := session.Run(
+		map[tf.Output]*tf.Tensor{
+			inputOp.Output(0): input,
+		},
+		[]tf.Output{
+			o1.Output(0),
+			o2.Output(0),
+			o3.Output(0),
+			o4.Output(0),
+		},
+		nil)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	allProbabilities := output[1].Value().([][]float32)
+	probabilities = allProbabilities[0]
+	classes = output[2].Value().([][]float32)[0]
+	boxes = output[0].Value().([][][]float32)[0]
+
+	return
 }
 
 func FilterByMinConfidence(
