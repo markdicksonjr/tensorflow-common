f6891e12cdf392b6dfaae01a193787f54c5d6c3c
Mark Dickson Jr <markdicksonjr@gmail.com>
2020-01-05 15:01:28 -0500

    Ensured empty labels and extra whitespace don't get loaded for one-per-line label files

--- labels.go
+++ labels.go
@@ -3,6 +3,7 @@ package tensorflow_common
 import (
 	"bufio"
 	"os"
+	"strings"
 )
 
 // LoadLabelsFileOnePerLine loads labels from a file, such that they are ordered and each is on a line by itself
@@ -15,7 +16,10 @@ func LoadLabelsFileOnePerLine(labelsFile string) ([]string, error) {
 	scanner := bufio.NewScanner(file)
 	var labels []string
 	for scanner.Scan() {
-		labels = append(labels, scanner.Text())
+		text := strings.TrimSpace(scanner.Text())
+		if len(text) > 0 {
+			labels = append(labels, text)
+		}
 	}
 	if err := scanner.Err(); err != nil {
 		return nil, err
